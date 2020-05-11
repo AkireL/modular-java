6365cf1d6b5db3590e36a6077abd2401c0619122
Leo <leonor.basurto@tanaholdings.com>
2020-05-11 01:30:29 -0500

    ejemplo practico de modulos en java 14

---
+++ JavaModularApplication1/src/mymodule/classes/com/greeting/Main.java
@@ -0,0 +1,7 @@
+package com.greeting;
+import com.names.Names;
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Taller de Módulos yo: "+ Names.getName());
+    }
+}
---
+++ JavaModularApplication1/src/othermodule/classes/module-info.java
@@ -0,0 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+module othermodule {
+    exports com.names; // 5 control de acceso es exponerlo apartir de java 9
+}
